a94c04d344fb063bce7ac891f28ca32740b36528
poll44 <faiz.ihsan.akram@gmail.com>
2024-05-24 19:54:09 +0700

    add service.blade.php
    penambahan:
    - folder layout isinya ada file baru untuk memudahkan kita jika ingin menambahkan page baru karena udah ada header dan footer
    - folder baru partials, isinya ada file untuk mengubah footer, header dst
    - perubahan di routes web.php, untuk title otomatis

--- routes/web.php
+++ routes/web.php
@@ -3,21 +3,21 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('home');
+    return view('home',["title"=>"Home"]);
 });
 
 Route::get('/division', function () {
-    return view('division');
+    return view('division', ["title"=>"Division"]);
 });
 
 Route::get('/relation', function () {
-    return view('relation');
+    return view('relation', ["title"=>"Relation"]);
 });
 
 Route::get('/publication', function () {
-    return view('publication');
+    return view('publication', ["title"=>"Publication"]);
 });
 
 Route::get('/service', function () {
-    return view('service');
+    return view('service', ["title"=>"Service"]);
 });
